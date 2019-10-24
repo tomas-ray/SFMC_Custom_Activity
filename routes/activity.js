5fdda0c3f1243c7aaef00a5dd1323710d1a56f88
juan.francia@prudential.com.sg <juan.francia@prudential.com.sg>
2019-10-24 18:48:03 +0800

    Update

--- routes/activity.js
+++ routes/activity.js
@@ -39,14 +39,16 @@ function logData(req) {
 }
 
 function writeToFile(output){
-
     var today = new Date();
     var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
-    fs.appendFile("./home/appuser/logs/jslogs.txt", output + '-' + date + ' - ' +time +"\r\n", function(err) {
+    fs.appendFile("./home/appuser/logs/jslogs.txt", date + '-' +time + output + "\r\n", function(err) {
        if(err) {
            return console.log(err);
+       }
+       else{
+           console.log('jslogs updated');
        }
    });  
 }
